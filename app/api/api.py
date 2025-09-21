ad63a19a5fd0ce5cd3155a401bfa5f0406bcfca1
Pranali Baviskar <123812793+Pranali-0104@users.noreply.github.com>
2025-09-21 22:50:48 +0530

    Delete app directory

--- app/api/api.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-from app.api.endpoints import videos,jobs,overlays
-
-api_router = APIRouter()
-api_router.include_router(videos.router)
-api_router.include_router(jobs.router)
-api_router.include_router(overlays.router)
--- app/core/config.py
+++
@@ -1,8 +0,0 @@
-# app/core/config.py
-
-import os
-
-class Settings:
-    DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:Admin@localhost:5432/fastapi_db")
-
-settings = Settings()
